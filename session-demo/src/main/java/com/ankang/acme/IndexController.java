4ec0748d81b4496396686aa03ccf4a226a12a208
张安康 <zhangankang@gwssi.com.cn>
2020-07-28 16:26:47 +0800

    新增

---
+++ session-demo/src/main/java/com/ankang/acme/IndexController.java
@@ -0,0 +1,17 @@
+package com.ankang.acme;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class IndexController {
+    @GetMapping("/index")
+    public String index(HttpServletRequest request) {
+        request.getSession().setAttribute("ankang","value");
+        return "success";
+    }
+    
+    
+}
